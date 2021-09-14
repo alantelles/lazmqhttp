9e29a946943fcdc674cc8cd112d498b0201df163
Alan Telles <alantelles86@gmail.com>
2021-09-14 20:40:30 -0300

    feature: fisrt well succedded implementation

--- lazmqexample.lpr
+++ lazmqexample.lpr
@@ -8,7 +8,11 @@ uses
   {$ENDIF}{$ENDIF}
   Classes, lazmqhttp
   { you can add units after this };
-
+var
+  Cli: TLazMqHttpClient;
 begin
+  Cli := TLazMqHttpClient.Create('lazmqdevexc', 'local.libdev.queue', 'local_lazmq_route', 'lazmqdevhost');
+  writeln(Cli.Publish('zika'));
+  Cli.Free;
 end.
 
--- src/lazmqhttp.pas
+++ src/lazmqhttp.pas
@@ -15,29 +15,50 @@ type
       FExchange: string;
       FQueue: string;
       FRoutingKey: string;
+      FVhost: string;
     public
       property Exchange: string read FExchange write FExchange;
       property Queue: string read FQueue write FQueue;
       property RoutingKey: string read FRoutingKey write FRoutingKey;
-      constructor Create(AExchange: string; AQueue: string; ARoutingKey: string);
-      function Publish(Payload: string; PayloadEncoding: TPayloadEncoding): boolean;
+      property VHost: string read FVhost write FVhost;
+      constructor Create(AExchange: string; AQueue: string; ARoutingKey: string; AVhost: string);
+      function Publish(Payload: string; PayloadEncodingIsString: boolean=True): boolean;
   end;
 
 
 implementation
 
-uses fphttpclient;
+uses fphttpclient, StrUtils;
 
-constructor TLazMqHttpClient.Create(AExchange: string; AQueue: string; ARoutingKey: string);
+constructor TLazMqHttpClient.Create(AExchange: string; AQueue: string; ARoutingKey: string; AVhost: string);
 begin
   FExchange := AExchange;
   FQueue := AQueue;
   FRoutingKey := ARoutingKey;
+  FVhost := AVhost;
 end;
 
-function TLazMqHttpClient.Publish(Payload: string; PayloadEncoding: TPayloadEncoding): boolean;
+function TLazMqHttpClient.Publish(Payload: string; PayloadEncodingIsString: boolean=True): boolean;
+var
+  HttpClient: TFpHttpClient;
+  StrStream: TStringStream;
+  Response, FullPayload: string;
+  PayloadAsString: string = 'base64';
 begin
-  Result := False;
+  HttpClient := TFPHttpClient.Create(nil);
+  HttpClient.UserName := 'guest';
+  HttpClient.Password := 'guest';
+  if PayloadencodingIsString then
+    PayloadAsString := 'string';
+  FullPayload := '{"properties": {}, "routing_key": "' + FRoutingKey + '", "payload": "' + ReplaceStr(Payload, '"', '\"') + '", "payload_encoding" : "' + PayloadAsString + '"}';
+  StrStream := TStringStream.Create(FullPayload);
+  HttpClient.RequestBody := StrStream;
+  HttpClient.AddHeader('Content-Type', 'application/json');
+  Response := HttpClient.Post('http://localhost:15672/api/exchanges/' + FVhost + '/' + FExchange + '/publish');
+  if Response = '{"routed":true}' then
+    Result := True
+  else
+    Result := False;
 end;
 
 end.
